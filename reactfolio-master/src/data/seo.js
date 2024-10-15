407b1bf5d9add08e9ed841f3b2b67135775ec924
rahul97701 <singh.rahulsingh.om07@gmail.com>
2024-10-15 12:26:27 -0400

    "my-portfolio"

---
+++ reactfolio-master/src/data/seo.js
@@ -0,0 +1,39 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a Frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Rahul", "Rahul Khichi", "Rahuul Khiichi"],
+	},
+
+	{
+		page: "about",
+		description:
+		    "I am a Frontend developer with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Rahul", "Rahul Khichi", "Rahuul Khiichi"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Rahul", "Rahul Khichi", "Rahuul Khiichi"],
+		},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Rahul", "Rahul Khichi", "Rahuul Khiichi"],
+
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Rahul", "Rahul Khichi", "Rahuul Khiichi"],
+	},
+];
+
+export default SEO;
